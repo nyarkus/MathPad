77df35089bbc3991f50a4cc7133b8fe965f2d897
nyarkus_ <klimenkoe2008@gmail.com>
2024-10-12 15:13:12 +0300

    license header fix :/

--- MathPad/RichTextBoxExstention.cs
+++ MathPad/RichTextBoxExstention.cs
@@ -1,5 +1,4 @@
-﻿// MathPad Copyright (C) 2024 Yahor Klimenko, All Rights Reserved.
-// Licensed under the MIT License.
+﻿// MathPad Copyright (C) 2024 Yahor Klimenko. All Rights Reserved. Licensed under the MIT License.  See License in the project root for license information.
 
 using System;
 using System.Collections.Generic;
